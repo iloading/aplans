a2e4cfe418669b26522379cb5b96647f6be37140
Samuel Santos <48221347+iloading@users.noreply.github.com>
2020-05-30 19:02:04 +0100

    Update estrutura index

--- components/cp_index_mobile.php
+++ components/cp_index_mobile.php
@@ -1,6 +1,13 @@
-<div class="container">
+<div class="container p-3">
     <section class="row">
-        <div class="col-6 text-left">olá</div>
-        <div class="col-6 text-right">olá</div>
+        <div class="col-6 text-left bg-warning">LOGO</div>
+        <div class="col-6 text-right bg-primary">USER ICON</div>
+    </section>
+</div>
+<div class="container p-3">
+    <section class="row pl-3"><p>My plans</p></section>
+    <section class="row">
+        <div class="col-3 bg-success">ADD ICON</div>
+        <div class="col-9 text-center bg-danger">EVENT / EVENTS</div>
     </section>
 </div>
