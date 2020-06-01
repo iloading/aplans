ff75767f35c8b618914e90be87946132976a87cd
Kengaz <david.riboira@gmail.com>
2020-06-01 18:15:04 +0100

    Progresso Settings

--- settings.php
+++ settings.php
@@ -25,14 +25,18 @@
 
   <main class="alturaMain text-black bg-white d-flex flex-column align-items-start">
     <article class="container p-0 mb-5 mainCentro">
-      <section class="row mt-4 mb-4 ml-0 pl-3 pl-sm-0">
-        <div class="col-2 col-sm-1 text-center pr-0 pl-0">
+      <section class="row mt-4 mb-4 ml-0 pl-sm-0">
+        <div class="col-2 col-sm-1 text-center pr-0 pl-0 pt-1">
           <img src="imagens/settings/back.png" width="30" height="30">
         </div>
 
-        <div class="col-9 col-sm-11 align-self-center pl-3 pl-sm-5 pr-0">
-          <p class="m-0 h1 text-left tituloTerms">Outdoor Cinema</p>
+        <div class="col-8 col-sm-10 align-self-center pl-sm-5 pr-0">
+          <p class="m-0 h1 text-left tituloTerms font-weight-bold">Outdoor Cinema</p>
 
+        </div>
+
+        <div class="col-1 col-sm-1 text-center pr-0 pl-2 pt-1">
+          <img src="imagens/settings/trash.png" width="30" height="30">
         </div>
 
       </section>
@@ -41,175 +45,137 @@
 
       <div class="container-fluid p-0">
 
-        <h4 class="settingsTitulo bg-preto m-0 p-3">Video</h4>
+        <div>
 
-        <div class="bg-trueBlack">
-
-          <div class="container-fluid">
+        <div class="dropdown-divider m-0"></div>
+          <div>
             <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Offline Mode</p>
-                <p class="settingsInfo m-0">Going offline for 6 hours</p>
+              <div class="p-4 ml-4 d-flex align-items-center">
+                <img src="imagens/settings/clock.png" width="25" height="25">
+              
               </div>
 
-              <div class="col-6 p-3 d-flex align-items-center justify-content-end">
-                <label class="switch m-0">
-                  <input type="checkbox">
-                  <span class="slider round"></span>
-                </label>
-              </div>
+              <div class="p-3 d-flex align-items-center">
+                <p class="settingsTituloTab m-0">10 pm, august 4, 2020</p>
             </div>
           </div>
 
           <div class="dropdown-divider m-0"></div>
 
-          <div class="container-fluid">
+          
+          <div>
             <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Subtitles</p>
-                <p class="settingsInfo m-0">Change subtitles size</p>
+              <div class="p-4 ml-4 d-flex align-items-center">
+                <img src="imagens/settings/location.png" width="25" height="25">
+              
               </div>
 
-              <div class="col-6 p-3 text-right">
-                <select class="mdb-select md-form">
-                  <option value="" disabled selected>Default</option>
-                  <option value="1">Small</option>
-                  <option value="2">Medium</option>
-                  <option value="3">Large</option>
-                </select>
-              </div>
-
+              <div class="p-3 d-flex align-items-center">
+                <p class="settingsTituloTab m-0">Key Gardens</p>
             </div>
           </div>
 
           <div class="dropdown-divider m-0"></div>
 
-          <div class="container-fluid">
+          <div>
             <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">High Definition</p>
-                <p class="settingsInfo m-0">Enable high definition streaming </p>
+              <div class="p-4 ml-4 d-flex align-items-center">
+                <img src="imagens/settings/team.png" width="25" height="25">
+              
               </div>
 
-              <div class="col-6 p-3 d-flex align-items-center justify-content-end">
-                <label class="switch m-0">
-                  <input type="checkbox">
-                  <span class="slider round"></span>
-                </label>
-              </div>
+              <div class="p-3 d-flex align-items-center">
+                <p class="settingsTituloTab m-0">10</p>
+            </div>
+            <div class="p-3 d-flex align-items-center ml-auto mr-4">
+              <button type="button" class="p-1" style="border: none; border-radius: 5px; font-size: 1rem">Edit participants <img src="imagens/settings/draw.png" width="18" height="17" class="pl-1"></button>
             </div>
           </div>
 
           <div class="dropdown-divider m-0"></div>
 
-          <div class="container-fluid">
+          <div>
             <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Cellular Data</p>
-                <p class="settingsInfo m-0">Allow the use off cellular data for streaming</p>
+              <div class="p-4 ml-4 d-flex">
+                <img src="imagens/settings/list.png" width="25" height="25">
+              
               </div>
-
-              <div class="col-6 p-3 text-right">
-                <select class="mdb-select md-form">
-                  <option value="" disabled selected>Default</option>
-                  <option value="1">Off</option>
-                  <option value="2">Automatic</option>
-                  <option value="3">Wi-fi Only</option>
-                </select>
-
-              </div>
-
+              <div>
+                <div class="pl-3 pt-4 d-flex align-items-center">
+                  <p class="settingsTituloTab m-0">Add a task</p>
+                </div>
+                <div class="pl-3 pt-1 d-flex align-items-center">
+                  <p class="settingsTituloTab m-0" style="font-size: 1rem">Description</p>
+                </div>
+              </div> 
+              <div class="p-3 d-flex align-items-center ml-auto mr-4">
+                <img src="imagens/settings/add.png" width="22" height="20" class="pl-1">
             </div>
           </div>
 
-          <div class="dropdown-divider m-0"></div>
-
-          <div class="container-fluid">
+          <div>
             <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Notifications</p>
-                <p class="settingsInfo m-0">Enable notifications while streaming</p>
+              <div class="p-4 ml-4 d-flex">
+                <img src="imagens/settings/list.png" width="25" height="25" style="display:none;">
+              
               </div>
-
-              <div class="col-6 p-3 d-flex align-items-center justify-content-end">
-                <label class="switch m-0">
-                  <input type="checkbox">
-                  <span class="slider round"></span>
-                </label>
-              </div>
+              <div class="ml-4 pt-4">
+                <div class="pl-3  d-flex align-items-center"  style="border-left: 3px solid red">
+                  <p class="settingsTituloTab m-0" style="color:red">Popcorn</p>
+                </div>
+                <div class="pl-3 pt-1 d-flex align-items-center"  style="border-left: 3px solid red">
+                  <p class="settingsTituloTab m-0" style="font-size: 1rem">5 buckets needed</p>
+                </div>
+              </div> 
+              <div class="p-3 d-flex align-items-center ml-auto mr-4">
+                <img src="imagens/settings/minus.png" width="22" height="20" class="pl-1">
             </div>
           </div>
+
+          <div>
+            <div class="row">
+              <div class="p-4 ml-4 d-flex">
+                <img src="imagens/settings/list.png" width="25" height="25" style="display:none;">
+              
+              </div>
+              <div class="ml-4 pt-4">
+                <div class="pl-3  d-flex align-items-center"  style="border-left: 3px solid blue">
+                  <p class="settingsTituloTab m-0" style="color:blue">Drinks</p>
+                </div>
+                <div class="pl-3 pt-1 d-flex align-items-center"  style="border-left: 3px solid blue">
+                  <p class="settingsTituloTab m-0" style="font-size: 1rem">Bringing the alchoolic beverages</p>
+                </div>
+              </div> 
+              <div class="p-3 d-flex align-items-center ml-auto mr-4">
+                <img src="imagens/settings/minus.png" width="22" height="20" class="pl-1">
+            </div>
+          </div>
+
+          <div>
+            <div class="row">
+              <div class="p-4 ml-4 d-flex">
+                <img src="imagens/settings/list.png" width="25" height="25" style="display:none;">
+              
+              </div>
+              <div class="ml-4 pt-4">
+                <div class="pl-3  d-flex align-items-center"  style="border-left: 3px solid green">
+                  <p class="settingsTituloTab m-0" style="color:green">White Screen</p>
+                </div>
+                <div class="pl-3 pt-1 d-flex align-items-center"  style="border-left: 3px solid green">
+                  <p class="settingsTituloTab m-0" style="font-size: 1rem">A big one</p>
+                </div>
+              </div> 
+              <div class="p-3 d-flex align-items-center ml-auto mr-4">
+                <img src="imagens/settings/minus.png" width="22" height="20" class="pl-1">
+            </div>
+          </div>
+
+          
 
         </div>
 
       </div>
-      <div class="mt-4 container-fluid p-0">
-
-        <h4 class="settingsTitulo bg-preto m-0 p-3">Audio</h4>
-
-        <div class="bg-trueBlack">
-
-
-
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Normalize</p>
-                <p class="settingsInfo m-0">Normalize your streaming audio</p>
-              </div>
-
-              <div class="col-6 p-3 d-flex align-items-center justify-content-end">
-                <label class="switch m-0">
-                  <input type="checkbox">
-                  <span class="slider round"></span>
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <div class="dropdown-divider m-0"></div>
-
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Autoplay</p>
-                <p class="settingsInfo m-0">Start playing next episode automatically</p>
-              </div>
-
-              <div class="col-6 p-3 d-flex align-items-center justify-content-end">
-                <label class="switch m-0">
-                  <input type="checkbox">
-                  <span class="slider round"></span>
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <div class="dropdown-divider m-0"></div>
-
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-6 p-3">
-                <p class="settingsTituloTab m-0">Streaming</p>
-                <p class="settingsInfo m-0">Streaming audio quality</p>
-              </div>
-
-              <div class="col-6 p-3 text-right dropUp">
-                <select class="mdb-select md-form">
-                  <option value="" disabled selected>Default</option>
-                  <option value="1">Low</option>
-                  <option value="2">High</option>
-                </select>
-
-              </div>
-
-            </div>
-
-          </div>
-
-        </div>
-
-      </div>
+      
     </article>
 
     <?php
