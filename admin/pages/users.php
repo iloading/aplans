73aa510c6154514223361142ef78feb2aed07e64
Samuel Santos <48221347+iloading@users.noreply.github.com>
2020-06-12 23:58:36 +0100

    mudei o titulo

--- admin/pages/users.php
+++ admin/pages/users.php
@@ -15,7 +15,7 @@ require_once "../scripts/sc_check_admin.php";
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>TESTE1</title>
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
