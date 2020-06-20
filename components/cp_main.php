8ff8c267457480c43559ec641f9b4e6ba34d1767
MiguelvFilipe <60987760+MiguelvFilipe@users.noreply.github.com>
2020-06-20 19:26:29 +0100

    main titulo e imagem

---
+++ components/cp_main.php
@@ -0,0 +1,87 @@
+ <main class="topRedondo bg-index d-flex flex-column align-items-start">
+
+     <article class="container d-flex flex-column p-0 mainCentro mb-5 alturaMain">
+
+         <div class="d-flex align-itens-center justify-content-between">
+             <div class="d-flex flex-column mr-3">
+                 <div>
+                     <h1 class="font-weight-bold display-3 text-azul-aplans">Outdoor Cinema</h1>
+                 </div>
+
+                 <div>
+                     <h2 class="text-secondary">10 pm, august 4, 2020</h2>
+                 </div>
+             </div>
+
+             <div class=" avatar-circular">
+                 <img src="assets/avatar.png">
+             </div>
+
+         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- <section class="row m-4 mb-4 ml-0 pl-sm-0 pb-4">
+
+
+
+             <div class="col-9">
+                 <div class="container-fluid">
+                     <div class="row">
+                         <h1 class="col-10 font-weight-bold display-3 pl-4 text-azul-aplans">Outdoor Cinema</h1>
+                     </div>
+
+                     <div class="row">
+                         <h2 class="col-10 pl-4 text-secondary">10 pm, august 4, 2020</h2>
+                     </div>
+                 </div>
+             </div>
+
+             <div class="col-3">
+                 <div class=" avatar-circular">
+                     <img src="assets/avatar.png">
+                 </div>
+             </div>
+
+
+
+             <!-- <div class="col-12 col-sm-10 align-self-center p-0 pl-xl-3 d-flex mt-4">
+                 <h2 class="col-10 font-weight-bold display-3 pl-4 text-azul-aplans">Outdoor Cinema</h2>
+                 <div class=" avatar-circular">
+                     <img src="assets/avatar.png">
+                 </div>
+             </div>
+
+             <div class="col-12 col-sm-10 align-self-center p-0 pl-xl-3 d-flex mt-3">
+                 <h2 class="col-10 pl-4 text-secondary">10 pm, august 4, 2020</h2>
+             </div> 
+
+         </section>
+
+         <section class="row m-4 mb-4 ml-0 pl-sm-0">
+
+             <div class="col-12 d-flex text-center divBotoesMargin border-top border-bottom">
+                 <a href="#" class="col-3 p-4 botoesMain">MAIN</a>
+                 <a href="#" class="col-3 p-4 botoesMain">PEOPLE</a>
+                 <a href="#" class="col-3 p-4 botoesMain">TASKS</a>
+                 <a href="#" class="col-3 p-4 botoesMain">CHAT</a>
+
+             </div>
+
+         </section> -->
+
+     </article>
+
+ </main>
