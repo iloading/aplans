1bd7e8f4aff2a4ea1739fa0d3a0015f479d7acde
Samuel Santos <48221347+iloading@users.noreply.github.com>
2020-07-02 12:15:14 +0100

    Limpar css

--- helpers/help_css.php
+++ helpers/help_css.php
@@ -10,7 +10,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 <!-- Stylesheets -->
-<link rel="stylesheet" href="css/bootstrap.min.css">
+
 <link rel="stylesheet" href="css/plugins.css">
 <link rel="stylesheet" href="style.css">
 <link href="admin/css/sb-admin-2.min.css" rel="stylesheet">
