a92362a477f92447f114821085ee1477cdd9b971
MiguelvFilipe <60987760+MiguelvFilipe@users.noreply.github.com>
2020-07-14 16:11:40 +0100

    nada feito

--- profile.php
+++ profile.php
@@ -22,127 +22,43 @@
   <!--/NAV-->
 
   <main class="text-black bg-white d-flex flex-column align-items-start topRedondo">
-    <article class="container p-0 mb-5 mainCentro">
-      <section class="row mt-5 mb-2 ml-5 pl-sm-0">
 
-        <div class="col-8 col-sm-10 align-self-center pl-0">
-          <p class="m-0 h1 text-left tituloTerms font-weight-normal" style="font-size: 2rem; color: #3C5E77">My Profile</p>
 
+
+
+
+
+    <article class="container d-flex flex-column pl-3 pr-3 pt-4 pb-4" style="max-width:inherit; background-color: #3C5E77;">
+      <div class="align-items-center d-flex">
+        <div class="d-flex align-items-center pr-3 ">
+          <img style="width: 1.1rem; height:1.1rem" src="images/others/back (5).png">
         </div>
-
-        <div class="col-4 col-sm-1 pr-0 pl-2 pt-1 d-flex align-items-center justify-content-center">
-          <img src="images/profile/more.png" width="30" height="30">
+        <div class=" avatar-circular">
+          <img src="assets/avatar.png">
         </div>
-
-      </section>
-
-
-
-      <div class="container-fluid p-0">
-
-        
-
-        
-            <div class="row mb-4 pt-3">
-              <div class="pl-4 pt-4 pb-4 pr-0 ml-4 d-flex align-items-center col-3">
-                <div class=" avatar-circular">
-                     <img src="assets/avatar.png">
-                </div>
-              
-              </div>
-
-              <div class="pl-3 d-flex align-items-center col-6">
-                <div class="row">
-                    <h5 class="col-12">Miguel Costa</h5>
-                    <p class="col-12" style="margin-bottom: 8px;">22, student</p>
-                    <div class="col-12">
-                        <img src="images/profile/starfill.png" width="16" height="16">
-                        <img src="images/profile/starfill.png" width="16" height="16">
-                        <img src="images/profile/starfill.png" width="16" height="16">
-                        <img src="images/profile/starfill.png" width="16" height="16">
-                        <img src="images/profile/star.png" width="16" height="16">
-                        
-                    </div>
-                </div>
-            </div>
-          </div>
-
-          
-
-          
+        <div class="d-flex flex-column pl-3">
+          <h5 class="font-weight-bold text-branco-evento m-0">Casandra Kalafa</h5>
+          <p class="m-0 text-branco-evento" style="margin-bottom: 8px;">22, student</p>
           <div>
-            <div class="row">
-              <div class="col-12 p-4 ml-4 d-flex align-items-center">
-                <h5>About me</h5>
-              </div>
-
-              <div class="col-11 pt-0 pl-4 pr-4 ml-4 d-flex align-items-center">
-                <p class="font-weight-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie maecenas tempus turpis consectetur pharetra. Leo eget neque placerat eget sed. Vitae est fusce risus, vitae magna pulvinar eget. Pharetra  dominum viverra egestas convallis purus sodales ac dolor, et cursus. Amet sit magna massa bibendum in quis.</p>
-              </div>
+            <img src="images/profile/starfill.png" width="16" height="16">
+            <img src="images/profile/starfill.png" width="16" height="16">
+            <img src="images/profile/starfill.png" width="16" height="16">
+            <img src="images/profile/starfill.png" width="16" height="16">
+            <img src="images/profile/star.png" width="16" height="16">
           </div>
-
-          
-
-          <div>
-            <div class="row">
-                <div class="col-12 pl-4 pr-4 pb-2 pt-2 ml-4 d-flex align-items-center">
-                    <h5>Preferences</h5>
-                    <div class="p-3 d-flex align-items-center ml-auto mr-4">
-                        <button type="button" class="p-1" style="border: none; border-radius: 5px; font-size: 1rem">Edit <img src="images/settings/draw.png" width="18" height="17" class="pl-1"></button>
-                    </div>
-                </div>
-
-                <div class="col-12 pt-0 pl-4 pr-4 ml-4 ">
-                    <p class="font-weight-light mb-2">Sports Plans</p>
-                    
-                    <div id="myProgress1">
-                        <div id="myBar1"></div>
-                    </div>
-                </div>
-
-                <div class="col-12 pt-0 pl-4 pr-4 ml-4 mt-4">
-                    <p class="font-weight-light mb-2">Culinary Plans</p>
-                    
-                    <div id="myProgress2">
-                        <div id="myBar2"></div>
-                    </div>
-                </div>
-
-                <div class="col-12 pt-0 pl-4 pr-4 ml-4 mt-4">
-                    <p class="font-weight-light mb-2">Musical Plans</p>
-                    
-                    <div id="myProgress3">
-                        <div id="myBar3"></div>
-                    </div>
-                </div>
-
-                <div class="col-12 pb-4 pt-0 pl-4 pr-4 ml-4 mt-4">
-                    <p class="font-weight-light mb-2">Cinema Plans</p>
-                    
-                    <div id="myProgress4">
-                        <div id="myBar4"></div>
-                    </div>
-                </div>
-                
-                
-                
-               
-
-            </div>
-            
-          </div>
-
-
-        
-
+        </div>
       </div>
-      
     </article>
+
+    <div class="container p-0 mb-5  d-flex flex-column pl-3 pr-3 pt-4 pb-4" style="height:100vh;">
 
     
 
-  </main>
+    </div>
 
+
+
+  </main>
 
   <?php
   require_once "helpers/help_js.php"
@@ -152,6 +68,101 @@
 
 
 
+
+
+
 </body>
 
 </html>
+
+
+<!--
+<div class="container p-0 mb-5  d-flex flex-column pl-3 pr-3 pt-4 pb-4" style="height:100vh;">
+
+  <div class="container-fluid p-0">
+
+
+    <div class="row">
+      <div class="p-4 ml-4 d-flex align-items-center">
+        <h5>About me</h5>
+      </div>
+
+      <div class="pt-0 pl-4 pr-4 ml-4 d-flex align-items-center">
+        <p class="font-weight-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie maecenas tempus turpis consectetur pharetra. Leo eget neque placerat eget sed. Vitae est fusce risus, vitae magna pulvinar eget. Pharetra dominum viverra egestas convallis purus sodales ac dolor, et cursus. Amet sit magna massa bibendum in quis.</p>
+      </div>
+    </div>
+
+    <div class="w-100 m-0 justify-content-center d-flex">
+
+
+      <div class="d-flex pl-4 pr-4" style="border:2px solid #BAE4AC;border-radius:16px">
+        <div class="">
+          <div class="botao-circular" style="margin-top: 50%;margin-bottom:50%;">+</div>
+        </div>
+
+        <div class="d-flex flex-column justify-content-center align-items-center pl-3">
+          <p class="d-flex align-items-center" style="font-weight: 600;margin:0;">És novo na aplicação?</p>
+          <p class="d-flex align-items-center" style="font-weight: 600;margin:0;">Adiciona novos amigos!</p>
+        </div>
+
+      </div>
+
+    </div>
+
+
+
+    <div>
+      <div class="row">
+        <div class="col-12 pl-4 pr-4 pb-2 pt-2 ml-4 d-flex align-items-center">
+          <h5>Preferences</h5>
+          <div class="p-3 d-flex align-items-center ml-auto mr-4">
+            <button type="button" class="p-1" style="border: none; border-radius: 5px; font-size: 1rem">Edit <img src="images/settings/draw.png" width="18" height="17" class="pl-1"></button>
+          </div>
+        </div>
+
+        <div class="col-12 pt-0 pl-4 pr-4 ml-4 ">
+          <p class="font-weight-light mb-2">Sports Plans</p>
+
+          <div id="myProgress1">
+            <div id="myBar1"></div>
+          </div>
+        </div>
+
+        <div class="col-12 pt-0 pl-4 pr-4 ml-4 mt-4">
+          <p class="font-weight-light mb-2">Culinary Plans</p>
+
+          <div id="myProgress2">
+            <div id="myBar2"></div>
+          </div>
+        </div>
+
+        <div class="col-12 pt-0 pl-4 pr-4 ml-4 mt-4">
+          <p class="font-weight-light mb-2">Musical Plans</p>
+
+          <div id="myProgress3">
+            <div id="myBar3"></div>
+          </div>
+        </div>
+
+        <div class="col-12 pb-4 pt-0 pl-4 pr-4 ml-4 mt-4">
+          <p class="font-weight-light mb-2">Cinema Plans</p>
+
+          <div id="myProgress4">
+            <div id="myBar4"></div>
+          </div>
+        </div>
+
+
+
+
+
+      </div>
+
+    </div>
+
+
+
+
+  </div>
+
+</div>-->
